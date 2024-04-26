0343ac208d0089c1c9c8bd83cf77426cd14a3a5f
EC2 Default User <ec2-user@ip-172-31-39-158.eu-north-1.compute.internal>
2024-04-26 06:28:16 +0000

    harsh

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, kubernetes!\n');
+  res.end('Hello, new teest 4!\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server runnings at http://${hostname}:${port}/`);
 });
